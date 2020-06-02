a3528ec43e2837bb5dce5d6e9ed8df91608df60d
Daniel Perelman <perelman@cs.washington.edu>
2020-06-01 19:04:52 -0700

    Add js/ to static/.

--- camera_site/settings.py
+++ camera_site/settings.py
@@ -79,7 +79,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/camera/static/'
 
 ASGI_APPLICATION = 'camera_site.routing.application'
 CHANNEL_LAYERS = {
